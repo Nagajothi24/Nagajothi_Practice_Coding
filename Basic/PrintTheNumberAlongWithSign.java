ba83c219935e9d0beb16a17c784b7012f3d2e0ae
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-12 12:00:14 +0530

    Create PrintTheNumberAlongWithSign.java

---
+++ Basic/PrintTheNumberAlongWithSign.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num=in.nextInt();
+        if(num>=0)
+            System.out.println("+"+num);
+        else
+            System.out.println(num);
+    }
+}
