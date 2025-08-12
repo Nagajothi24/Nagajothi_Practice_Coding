0a170b98ac1c0a48404687cf402aef9076f31fe2
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-12 12:19:00 +0530

    Create multiply_two_decimals.java

---
+++ Operators/multiply_two_decimals.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner in = new Scanner(System.in);
+        float n1=in.nextFloat();
+        float n2=in.nextFloat();
+        float mul=n1*n2;
+        System.out.printf("%.2f",mul);
+    }
+}
