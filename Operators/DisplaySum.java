0e2c2160abdb9bd48a02e79ac4f573efae6d5852
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-12 12:14:57 +0530

    Create DisplaySum.java

---
+++ Operators/DisplaySum.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n1=in.nextInt();
+        int n2=in.nextInt();
+        System.out.println("Sum of "+n1+" and "+n2+" is "+(n1+n2));
+    }
+}
