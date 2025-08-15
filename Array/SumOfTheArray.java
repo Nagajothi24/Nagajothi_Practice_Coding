24d0a6d29fc2595d70000bc6a7f7a7af86703287
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-15 22:23:23 +0530

    Create SumOfTheArray.java

---
+++ Array/SumOfTheArray.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    Scanner in = new Scanner(System.in);
+        int size=in.nextInt();
+        int []a=new int[size];
+        int sum=0;
+        for(int i=0;i<size;i++){
+            a[i]=in.nextInt();
+            sum=sum+a[i];
+        }
+        System.out.println(sum);
+    }
+}
