28d3ba74a81641758217b5e10370ba0f4fd94753
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-16 20:17:21 +0530

    Create DisplayPassword.java

---
+++ Strings/DisplayPassword.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    Scanner in = new Scanner(System.in);
+        String s=in.nextLine();
+        for(int i=0;i<s.length();i++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
