917585aa98d1e5c8af09a6d27b63db642fa0729b
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-16 20:30:42 +0530

    Create StarPattern4.java

---
+++ Patterns/StarPattern4.java
@@ -0,0 +1,38 @@
+// Sample Input :
+
+// 5
+
+// Sample Output :
+
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                if(row==1 || row==n || col==1 ||col==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
