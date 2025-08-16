5cfe342a9e434b1559cd630eae5e6c036f6c651f
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-16 20:23:26 +0530

    Create StarPattern2.java

---
+++ Patterns/StarPattern2.java
@@ -0,0 +1,31 @@
+// Sample Input :
+// 4
+
+// Sample Output :
+// ****
+// ****
+// ****
+// ****
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
