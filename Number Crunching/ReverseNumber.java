43d754f9d735383a8ca9325e99b0419fdf2c5ab5
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-19 22:16:39 +0530

    Create ReverseNumber.java

---
+++ Number Crunching/ReverseNumber.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+   Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long rev=0,ld;
+        while(n>0){
+            ld=n%10;
+            n=n/10;
+            rev=rev*10+ld;        }
+        System.out.print(rev);
+    }
+}
