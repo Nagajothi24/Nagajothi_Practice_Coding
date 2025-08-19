4c3bed55077e9bc27f7f486da0c46b23b8b40be2
Nagajothi24 <160407991+Nagajothi24@users.noreply.github.com>
2025-08-19 22:15:55 +0530

    Create DigitsRightToLeft.java

---
+++ Number Crunching/DigitsRightToLeft.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+     Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long ld,rev=0;
+        while(n>0){
+            ld=n%10;
+            n=n/10;
+        System.out.print(ld+" ");        }
+        
+        
+    }
+}
